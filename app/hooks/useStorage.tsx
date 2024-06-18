d0e5a4feae4905ef0fe358c6bf88322a1be41bc9
BraveVlad <giladvladpinker@gmail.com>
2024-06-19 01:22:14 +0300

    App/Storage - + created stub useStorage hook. + created stub get storage for native and web.

--- app/hooks/useStorage.tsx
+++ app/hooks/useStorage.tsx
@@ -1,1 +1,23 @@
-export default function useStorage() {}
+import { useEffect, useState } from "react";
+import { Platform } from "react-native";
+
+function getNativeStorage(key: string) {
+	return "";
+}
+
+function getWebStorage(key: string) {
+	return "";
+}
+
+export default function useStorage(key: string) {
+	const [storageState, setStorageState] = useState<string | null>();
+
+	useEffect(() => {
+		if (Platform.OS !== "web") {
+			// load from native storage using expo store module
+			getNativeStorage(key);
+		} else {
+			setStorageState(getWebStorage(key));
+		}
+	});
+}
