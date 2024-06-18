108ac8b642e9504c654274adbfeb61cb334c2154
BraveVlad <giladvladpinker@gmail.com>
2024-06-19 00:25:11 +0300

    App/Auth/useAuth - + added an 'Authentication' type. + creation of 'AuthContext' to share authentication globally across the app.

--- app/hooks/useAuth.tsx
+++ app/hooks/useAuth.tsx
@@ -1,1 +1,17 @@
+import { createContext } from "react";
+
+type Authentication = {
+	logIn: () => void;
+	logOut: () => void;
+	session?: string;
+	isLoading: boolean;
+};
+
+const AuthContext = createContext<Authentication>({
+	logIn: () => null,
+	logOut: () => null,
+	session: undefined,
+	isLoading: false,
+});
+
 export default function useAuth() {}
