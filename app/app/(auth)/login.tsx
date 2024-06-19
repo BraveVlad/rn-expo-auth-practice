47995d5620ab1e9f7ee4243a9ff81decd7245c0a
BraveVlad <giladvladpinker@gmail.com>
2024-06-19 05:14:06 +0300

    App/Auth/Login - + Login screen now has username input and login button

--- app/app/(auth)/login.tsx
+++ app/app/(auth)/login.tsx
@@ -1,11 +1,24 @@
-import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
+import { useState } from "react";
+import { Button, StyleSheet, Text, TextInput, View } from "react-native";
 
 export default function LoginScreen() {
+	const [username, setUsername] = useState<string>("");
+
+	function handleLogin(): void {
+		console.log(username);
+	}
+
 	return (
 		<View style={styles.container}>
 			<Text>Welcome to login!</Text>
-			<StatusBar style="auto" />
+			<TextInput
+				id="login-username"
+				placeholder="Type your username here."
+				value={username}
+				onChangeText={(text) => setUsername(text)}
+				autoComplete="username"
+			/>
+			<Button title="Log In" onPress={handleLogin} />
 		</View>
 	);
 }
