c8d3bdc5377cc98959bdb0a0ec2b0e6906b452f4
BraveVlad <giladvladpinker@gmail.com>
2024-06-19 05:22:05 +0300

    App/Auth/AuthRoute - + Auth route now redirects to main screen if already logged in.

--- app/app/(auth)/_layout.tsx
+++ app/app/(auth)/_layout.tsx
@@ -1,5 +1,17 @@
-import { Slot } from "expo-router";
+import { Redirect, Slot } from "expo-router";
+import { useSafeAreaFrame } from "react-native-safe-area-context";
+import { useAuth } from "../../hooks/useAuth";
+import { Text } from "react-native";
 
 export default function AuthLayout() {
+	const { session, isLoading } = useAuth();
+
+	if (isLoading) {
+		return <Text>Loading...</Text>;
+	}
+	if (session) {
+		return <Redirect href="/main/" />;
+	}
+
 	return <Slot />;
 }
