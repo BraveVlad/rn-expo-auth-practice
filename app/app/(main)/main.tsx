8696f6ed6154301a47953d62e24365b4765221d9
BraveVlad <giladvladpinker@gmail.com>
2024-06-19 04:33:58 +0300

    App/Auth - + Main Screen for logged in users now redirects back to public screen if use is not logged in.

--- app/app/(main)/main.tsx
+++ app/app/(main)/main.tsx
@@ -1,10 +1,21 @@
 import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, View } from "react-native";
+import { useAuth } from "../../hooks/useAuth";
+import { Redirect } from "expo-router";
 
 export default function MainApp() {
+	const { session, isLoading } = useAuth();
+
+	if (isLoading) {
+		return <Text>Loading...</Text>;
+	}
+
+	if (!session) {
+		return <Redirect href={""} />;
+	}
 	return (
 		<View style={styles.container}>
-			<Text>Welcome to Auth practice!</Text>
+			<Text>Welcome to logged in main screen!</Text>
 			<StatusBar style="auto" />
 		</View>
 	);
